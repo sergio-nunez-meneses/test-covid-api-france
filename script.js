3b67a5aca6266630a1c1bd14539364382ba7a4d8
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-01-29 16:48:44 +0100

    added chart.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var search = getBy('name', 'search'),
+var ctx = getBy('tag', 'canvas')[0].getContext('2d'),
+  search = getBy('name', 'search'),
   display = getBy('id', 'code');
 
 function getBy(attribute, value) {
@@ -11,6 +12,53 @@ function getBy(attribute, value) {
   } else if (attribute === 'class') {
     return document.getElementsByClassName(value);
   }
+}
+
+function randomRGBA() {
+  let color = 'rgba(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.random() + ')';
+
+  if (!(color < 'rgba(0, 0, 0, 1)' && color > 'rgba(0, 0, 0, 0)')) {
+    return color;
+  }
+}
+
+function chart(department, date, labels, data) {
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: labels,
+          datasets: [{
+              label: department + ' (au ' + date + ')',
+              data: data,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
 }
 
 function ajax(departement) {
@@ -26,16 +74,16 @@ function response() {
     return;
   }
 
-  var response = JSON.parse(this.responseText).LiveDataByDepartement[0];
-  display.innerHTML = JSON.stringify(response, undefined, 2);
-  console.log(response);
+  var response = JSON.parse(this.responseText).LiveDataByDepartement[0],
+    labels = ['guéris.es', 'hospitalisés.es', 'reanimation', 'décès', 'nouvelles hospitalisations', 'nouvelles reanimations'],
+    data = [response.gueris, response.hospitalises, response.reanimation, response.deces, response.nouvellesHospitalisations, response.nouvellesReanimations];
+
+  chart(response.nom, response.date, labels, data);
 }
 
 search.addEventListener('click', () => {
   var input = getBy('name', 'department').value,
     filteredInput;
-
-  // filter input
 
   ajax(input);
 });
