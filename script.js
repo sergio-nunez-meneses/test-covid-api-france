4984ce9fdef60f346355dcd6f447b74425927f63
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-02-01 14:22:11 +0100

    updated handleResponse function in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var departments = ['Ain', 'Aisne', 'Allier', 'Alpes-de-Haute-Provence', 'Hautes-Alpes', 'Alpes-Maritimes', 'Ardèche', 'Ardennes', 'Ariège', 'Aube', 'Aude', 'Aveyron', 'Bouches-du-Rhône', 'Calvados', 'Cantal', 'Charente', 'Charente-Maritime', 'Cher', 'Corrèze', 'Corse-du-Sud', 'Haute-Corse', "Côte-d'Or", "Côtes-d'Armor", 'Creuse', 'Dordogne', 'Doubs', 'Drôme', 'Eure', 'Eure-et-Loir', 'Finistère', 'Gard', 'Haute-Garonne', 'Gers', 'Gironde', 'Hérault', 'Ille-et-Vilaine', 'Indre', 'Indre-et-Loire', 'Isère', 'Jura', 'Landes', 'Loir-et-Cher', 'Loire', 'Haute-Loire', 'Loire-Atlantique', 'Loiret', 'Lot', 'Lot-et-Garonne', 'Lozère', 'Maine-et-Loire', 'Manche', 'Marne', 'Haute-Marne', 'Mayenne', 'Meurthe-et-Moselle', 'Meuse', 'Morbihan', 'Moselle', 'Nièvre', 'Nord', 'Oise', 'Orne', 'Pas-de-Calais', 'Puy-de-Dôme', 'Pyrénées-Atlantiques', 'Hautes-Pyrénées', 'Pyrénées-Orientales', 'Bas-Rhin', 'Haut-Rhin', 'RhôneNote', 'Haute-Saône', 'Saône-et-Loire', 'Sarthe', 'Savoie', 'Haute-Savoie', 'Paris', 'Seine-Maritime', 'Seine-et-Marne', 'Yvelines', 'Deux-Sèvres', 'Somme', 'Tarn', 'Tarn-et-Garonne', 'Var', 'Vaucluse', 'Vendée', 'Vienne', 'Haute-Vienne', 'Vosges', 'Yonne', 'Territoire de Belfort', 'Essonne', 'Hauts-de-Seine', 'Seine-Saint-Denis', 'Val-de-Marne', "Val-d'Oise", 'Guadeloupe', 'Martinique', 'Guyane', 'La Réunion', 'Mayotte'],
   buttons = getBy('tag', 'button'),
-  canvas = getBy('tag', 'canvas')[0],
-  ctx = canvas.getContext('2d'),
+  // canvas = getBy('tag', 'canvas')[0],
+  // ctx = canvas.getContext('2d'),
   colors = [],
   barsChart;
 
@@ -141,7 +141,7 @@ function request(request) {
 function handleResponse() {
   let response = JSON.parse(this.responseText),
     objectKey = Object.keys(response)[0],
-    rawData, dataKeys;
+    rawData, rawDataKeys, rawDataSize;
 
   if (objectKey === 'FranceGlobalLiveData') {
     rawData = response.FranceGlobalLiveData[0];
@@ -153,16 +153,26 @@ function handleResponse() {
     rawData = response.allFranceDataByDate;
   }
 
-  dataKeys = Object.keys(rawData);
-  filterData(rawData, dataKeys);
+  rawDataKeys = Object.keys(rawData);
+
+  if (rawDataKeys.length < 121) {
+    console.log('render');
+    filterData(rawData, rawDataKeys);
+  } else {
+    console.log('iterate and render');
+
+    for (var i = 0; i < rawData.length; i++) {
+      console.log(rawData[i], Object.keys(rawData[i]));
+    }
+  }
 }
 
-function filterData(data, dataKeys) {
+function filterData(data, keys) {
   var filteredData = [],
     labels = [],
     label;
 
-  dataKeys.forEach((key) => {
+  keys.forEach((key) => {
     if (key !== 'code' && key !== 'source' && key !== 'sourceType') {
       label = key.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => {
         return str.toUpperCase();
@@ -173,27 +183,21 @@ function filterData(data, dataKeys) {
     }
   });
 
-  console.log('labels', labels);
-  console.log('filtered data', filteredData);
+  generateColors(filteredData.length);
+
+  console.log(labels, filteredData, colors);
+  renderData();
 }
 
-function renderData(data) {
-  console.log(data);
-}
+function renderData() {
+  let canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    responseContainer = getBy('class', 'response-container')[0];
 
-// function response() {
-//   if (this.responseText.charAt(0) !== '{') {
-//     console.log('not JSON');
-//     return;
-//   }
-//
-//   var response = JSON.parse(this.responseText).LiveDataByDepartement[0],
-//     labels = ['guéris.es', 'hospitalisés.es', 'réanimation', 'décès', 'nouvelles hospitalisations', 'nouvelles réanimations'],
-//     data = [response.gueris, response.hospitalises, response.reanimation, response.deces, response.nouvellesHospitalisations, response.nouvellesReanimations];
-//
-//   generateColors(data.length);
-//   chart(response.nom, response.date, labels, data, colors);
-// }
+  // canvas.style.backgroundColor = '#000';
+  // responseContainer.appendChild(canvas);
+  // chart(response.nom, response.date, labels, data, colors);
+}
 
 // add event listener to all <button>
 for (let button of buttons) {
@@ -202,7 +206,7 @@ for (let button of buttons) {
   });
 }
 
-// populate <select> with french departments
+// populate <select> with departments array
 for (var i = 0; i < departments.length; i++) {
   var option = document.createElement('option');
   option.value = departments[i];
