dd474b70c6b11513e623b0fe7b7dd846f59b4843
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-02-01 22:17:15 +0100

    updated handleResponse function and working on renderData function in script.js; updated index.html and style.css

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var departments = ['Ain', 'Aisne', 'Allier', 'Alpes-de-Haute-Provence', 'Hautes-Alpes', 'Alpes-Maritimes', 'Ardèche', 'Ardennes', 'Ariège', 'Aube', 'Aude', 'Aveyron', 'Bouches-du-Rhône', 'Calvados', 'Cantal', 'Charente', 'Charente-Maritime', 'Cher', 'Corrèze', 'Corse-du-Sud', 'Haute-Corse', "Côte-d'Or", "Côtes-d'Armor", 'Creuse', 'Dordogne', 'Doubs', 'Drôme', 'Eure', 'Eure-et-Loir', 'Finistère', 'Gard', 'Haute-Garonne', 'Gers', 'Gironde', 'Hérault', 'Ille-et-Vilaine', 'Indre', 'Indre-et-Loire', 'Isère', 'Jura', 'Landes', 'Loir-et-Cher', 'Loire', 'Haute-Loire', 'Loire-Atlantique', 'Loiret', 'Lot', 'Lot-et-Garonne', 'Lozère', 'Maine-et-Loire', 'Manche', 'Marne', 'Haute-Marne', 'Mayenne', 'Meurthe-et-Moselle', 'Meuse', 'Morbihan', 'Moselle', 'Nièvre', 'Nord', 'Oise', 'Orne', 'Pas-de-Calais', 'Puy-de-Dôme', 'Pyrénées-Atlantiques', 'Hautes-Pyrénées', 'Pyrénées-Orientales', 'Bas-Rhin', 'Haut-Rhin', 'RhôneNote', 'Haute-Saône', 'Saône-et-Loire', 'Sarthe', 'Savoie', 'Haute-Savoie', 'Paris', 'Seine-Maritime', 'Seine-et-Marne', 'Yvelines', 'Deux-Sèvres', 'Somme', 'Tarn', 'Tarn-et-Garonne', 'Var', 'Vaucluse', 'Vendée', 'Vienne', 'Haute-Vienne', 'Vosges', 'Yonne', 'Territoire de Belfort', 'Essonne', 'Hauts-de-Seine', 'Seine-Saint-Denis', 'Val-de-Marne', "Val-d'Oise", 'Guadeloupe', 'Martinique', 'Guyane', 'La Réunion', 'Mayotte'],
   buttons = getBy('tag', 'button'),
-  // canvas = getBy('tag', 'canvas')[0],
-  // ctx = canvas.getContext('2d'),
   colors = [],
   barsChart;
 
@@ -124,7 +122,7 @@ function request(request) {
     url = 'https://coronavirusapi-france.now.sh/FranceLiveGlobalData';
   } else if (request === 'departments-data') {
     url = 'https://coronavirusapi-france.now.sh/AllLiveData';
-  } else if (request === 'search') {
+  } else if (request === 'department-data') {
     // let input = getBy('name', 'department').value;
     let selectedOption = getBy('tag', 'select')[0].selectedOptions[0].value;
 
@@ -141,7 +139,7 @@ function request(request) {
 function handleResponse() {
   let response = JSON.parse(this.responseText),
     objectKey = Object.keys(response)[0],
-    rawData, rawDataKeys, rawDataSize;
+    rawData, rawDataKeys;
 
   if (objectKey === 'FranceGlobalLiveData') {
     rawData = response.FranceGlobalLiveData[0];
@@ -156,13 +154,10 @@ function handleResponse() {
   rawDataKeys = Object.keys(rawData);
 
   if (rawDataKeys.length < 121) {
-    console.log('render');
     filterData(rawData, rawDataKeys);
   } else {
-    console.log('iterate and render');
-
     for (var i = 0; i < rawData.length; i++) {
-      console.log(rawData[i], Object.keys(rawData[i]));
+      filterData(rawData[i], Object.keys(rawData[i]));
     }
   }
 }
@@ -173,7 +168,7 @@ function filterData(data, keys) {
     label;
 
   keys.forEach((key) => {
-    if (key !== 'code' && key !== 'source' && key !== 'sourceType') {
+    if (key !== 'code' && key !== 'nom' && key !== 'source' && key !== 'sourceType') {
       label = key.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => {
         return str.toUpperCase();
       });
@@ -185,18 +180,19 @@ function filterData(data, keys) {
 
   generateColors(filteredData.length);
 
-  console.log(labels, filteredData, colors);
-  renderData();
+  console.log(data.nom, data.date, labels, filteredData, colors);
+  renderData(data.nom, data.date, labels, filteredData, colors);
 }
 
-function renderData() {
+function renderData(name, date, labels, data, colors) {
   let canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d'),
     responseContainer = getBy('class', 'response-container')[0];
 
-  // canvas.style.backgroundColor = '#000';
-  // responseContainer.appendChild(canvas);
-  // chart(response.nom, response.date, labels, data, colors);
+  // chart(ctx, name, date, labels, data, colors);
+  generateColors(1);
+  canvas.style.backgroundColor = colors[0];
+  responseContainer.appendChild(canvas);
 }
 
 // add event listener to all <button>
